6173ab4e3e9c2e136371556dac5cacfe1890be0c
sujahathmsm <sujahathmhmd@gmail.com>
2025-01-01 20:37:50 +0530

    Modified Auth - Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton>
+        <SignInButton mode="modal">
           <button className="bg-red-500">Sign in</button>
         </SignInButton>
       </SignedOut>
