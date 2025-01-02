d471a25c1e9c0cf8c287fefe3d63196083607db7
sujahathmsm <sujahathmhmd@gmail.com>
2025-01-02 22:16:15 +0530

    Building Navbar - 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { MailOpen } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
+
 export default function Home() {
   return (
     <div>
-      <ModeToggle />
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>
-            <MailOpen />
-            Sign In
-          </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      <h1>Progress</h1>
+      <Progress value={33} />
     </div>
   );
 }
